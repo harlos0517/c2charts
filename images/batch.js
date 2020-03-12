2b3f08dc5727b55a5b6b0d34115b1129d80b013e
DeemoHarlos <cces4415@gmail.com>
2020-03-13 02:12:16 +0800

    path

--- images/batch.js
+++ images/batch.js
@@ -4,6 +4,8 @@ const chart2images = require('./chart2images.js')
 
 const characters = require('./songs.json')
 
+const rootPath = 'output'
+
 function processChart(song, difficulty_id, character, path) {
 	// Check directory existance
 	if (!fs.existsSync(path))
@@ -12,8 +14,7 @@ function processChart(song, difficulty_id, character, path) {
 	let data = require(`./data/${character.id}_${song.id}_${difficulty_id}_decrypted.json`)
 
 	// check completeness
-	let complete = true
-	// 
+	let complete = true // set this to false and disable the following block to startover
 	for (let page_index = 0; page_index < data.page_list.length; page_index++) {
 		if (!fs.existsSync(`${path}/${`${page_index}`.padStart(3, '0')}.png`)) {
 			complete = false
@@ -36,17 +37,20 @@ function processSong(song, character, path) {
 
 function process() {
 	chart2images.loadAssets(()=>{
+		// Check directory existance
+		if (!fs.existsSync(rootPath))
+			fs.mkdirSync(rootPath)
 		characters.forEach(character=>{
 			// Check directory existance
-			if (!fs.existsSync(`output/${character.id}`))
-				fs.mkdirSync(`output/${character.id}`)
+			if (!fs.existsSync(`${rootPath}/${character.id}`))
+				fs.mkdirSync(`${rootPath}/${character.id}`)
 			character.songs.forEach(song=>{
 				// Check directory existance
-				if (!fs.existsSync(`output/${character.id}/${song.id}`))
-					fs.mkdirSync(`output/${character.id}/${song.id}`)
+				if (!fs.existsSync(`${rootPath}/${character.id}/${song.id}`))
+					fs.mkdirSync(`${rootPath}/${character.id}/${song.id}`)
 
 				console.log(`[ Process Song ] ${character.name} - ${song.name}`)
-				processSong(song, character, `output/${character.id}/${song.id}`)
+				processSong(song, character, `${rootPath}/${character.id}/${song.id}`)
 			})
 		})
 	})

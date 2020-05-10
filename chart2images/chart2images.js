4f99d5fa512023749220862fd4699b67f323351a
DeemoHarlos <cces4415@gmail.com>
2020-05-10 21:40:36 +0800

    some fixes

--- chart2images/chart2images.js
+++ chart2images/chart2images.js
@@ -46,9 +46,9 @@ function yPos(dir, ratio) {
 	return (dir === 1) ? (margin.top + noteAreaHeight - verDist) : (margin.top + verDist)
 }
 
-function drawBeatLine(ctx, dir, ratio, color) {
+function drawBeatLine(ctx, dir, ratio, color, lineWidth) {
 	let y = yPos(dir, ratio)
-	ctx.lineWidth = 1
+	ctx.lineWidth = lineWidth
 	ctx.strokeStyle = color
 	ctx.beginPath()
 	ctx.moveTo(0, y)
@@ -78,23 +78,24 @@ function initCanvas(ctx, page, data, tempo) {
 	let beats = (page.end_tick - page.start_tick) / data.time_base
 	let dir = page.scan_line_direction
 
-	let grdStart = (dir === 1) ? (margin.top + noteAreaHeight     ) : (margin.top     )
-	let grdEnd   = (dir === 1) ? (margin.top + noteAreaHeight - 40) : (margin.top + 40)
+	let thickness = 75
+	let grdStart = (dir === 1) ? (margin.top + noteAreaHeight            ) : (margin.top            )
+	let grdEnd   = (dir === 1) ? (margin.top + noteAreaHeight - thickness) : (margin.top + thickness)
 	let grd = ctx.createLinearGradient(0, grdStart, 0, grdEnd);
 	grd.addColorStop(0, `#909090FF`)
 	grd.addColorStop(1, `#90909000`)
 	ctx.fillStyle = grd
-	ctx.fillRect(0, (dir === 1) ? grdEnd : grdStart, width, 40)
+	ctx.fillRect(0, (dir === 1) ? grdEnd : grdStart, width, thickness)
 
 	for (let beat = 0; beat <= beats; beat++) {
-		drawBeatLine(ctx, dir, beat/beats, '#C7C7C7')
-		if (beat+0.5000 <= beats) drawBeatLine(ctx, dir, (beat+0.5000)/beats, '#787878')
-		if (beat+0.2500 <= beats) drawBeatLine(ctx, dir, (beat+0.2500)/beats, '#323232')
-		if (beat+0.7500 <= beats) drawBeatLine(ctx, dir, (beat+0.7500)/beats, '#323232')
-		if (beat+0.1667 <= beats) drawBeatLine(ctx, dir, (beat+0.1667)/beats, '#003254')
-		if (beat+0.3333 <= beats) drawBeatLine(ctx, dir, (beat+0.3333)/beats, '#003254')
-		if (beat+0.6667 <= beats) drawBeatLine(ctx, dir, (beat+0.6667)/beats, '#003254')
-		if (beat+0.8333 <= beats) drawBeatLine(ctx, dir, (beat+0.8333)/beats, '#003254')
+		drawBeatLine(ctx, dir, beat/beats, '#C7C7C7', 4)
+		if (beat+0.5000 <= beats) drawBeatLine(ctx, dir, (beat+0.5000)/beats, '#787878', 1)
+		// if (beat+0.2500 <= beats) drawBeatLine(ctx, dir, (beat+0.2500)/beats, '#323232', 2)
+		// if (beat+0.7500 <= beats) drawBeatLine(ctx, dir, (beat+0.7500)/beats, '#323232', 2)
+		// if (beat+0.1667 <= beats) drawBeatLine(ctx, dir, (beat+0.1667)/beats, '#003254', 1)
+		// if (beat+0.3333 <= beats) drawBeatLine(ctx, dir, (beat+0.3333)/beats, '#005486', 2)
+		// if (beat+0.6667 <= beats) drawBeatLine(ctx, dir, (beat+0.6667)/beats, '#005486', 2)
+		// if (beat+0.8333 <= beats) drawBeatLine(ctx, dir, (beat+0.8333)/beats, '#003254', 1)
 	}
 }
 
@@ -124,10 +125,10 @@ function drawLongHoldTail(note, page, ctx) {
 	let longHoldTail = noteType.longHoldTail
 
 	if (dir === 1) [verPos, verPosEnd] = [verPosEnd, verPos]
-	for (let point = verPos; point<verPosEnd; point += longHoldTail.height*noteRatio) {
+	for (let point = verPos; point<verPosEnd; point += longHoldTail.height*.8*noteRatio) {
 		ctx.drawImage(longHoldTail,
-			horPos - longHoldTail.width*noteRatio/2, point,
-			longHoldTail.width*noteRatio, Math.min(longHoldTail.height*noteRatio, verPosEnd - point)
+			horPos - longHoldTail.width*.8*noteRatio/2, point,
+			longHoldTail.width*.8*noteRatio, Math.min(longHoldTail.height*.8*noteRatio, verPosEnd - point)
 		)
 	}
 }
@@ -144,7 +145,43 @@ function drawLongHoldPoint(note, ctx) {
 	)
 }
 
-function drawNote(note, page, ctx) {
+function approx(a, b, err) {
+	return (Math.abs(a - b) < err)
+}
+
+function drawNoteLine(note, page, ctx, base) {
+	let horPos = margin.side + noteAreaWidth * note.x
+	let verPos = yPos(page.scan_line_direction, (note.tick-page.start_tick) / (page.end_tick-page.start_tick))
+	let tick = (note.tick-page.start_tick) % base
+	let length = (note.type === 4 || note.type === 7) ? 150 : ((note.type === 5) ? 400 : 300)
+	let color
+	     if (approx(tick*2, base  , 10)) color = '#787878'
+	else if (approx(tick*3, base  , 15) || approx(tick*3, base*2, 15)) color = '#0078A2'
+	else if (approx(tick*4, base  , 20) || approx(tick*4, base*3, 20)) color = '#787878'
+	else if (approx(tick*6, base  , 30) || approx(tick*6, base*5, 30)) color = '#003254'
+	else if (approx(tick*8, base*1, 40) || approx(tick*8, base*3, 40)) color = '#323232'
+	else if (approx(tick*8, base*5, 40) || approx(tick*8, base*7, 40)) color = '#323232'
+	else return
+	ctx.beginPath()
+	ctx.lineWidth = 4
+	ctx.strokeStyle = color
+	ctx.setLineDash([])
+	ctx.moveTo(horPos - length/2, verPos)
+	ctx.lineTo(horPos + length/2, verPos)
+	ctx.stroke()
+}
+
+function getRotate(note, page, nextNote, nextPage) {
+	let horPos = margin.side + noteAreaWidth * note.x
+	let verPos = yPos(page.scan_line_direction, (note.tick-page.start_tick) / (page.end_tick-page.start_tick))
+	let nextHorPos = margin.side + noteAreaWidth * nextNote.x
+	let nextVerPos = yPos(nextPage.scan_line_direction, (nextNote.tick-nextPage.start_tick) / (nextPage.end_tick-nextPage.start_tick))
+	let dx = nextHorPos - horPos
+	let dy = nextVerPos - verPos
+	return Math.atan2(dx, -dy)
+}
+
+function drawNote(note, page, ctx, rotate) {
 	let noteObj = noteType.notes
 	     if (note.type === 0) noteObj = noteType.note
 	else if (note.type === 1) noteObj = noteType.hold
@@ -156,13 +193,24 @@ function drawNote(note, page, ctx) {
 	else if (note.type === 7) noteObj = noteType.chain
 	else console.log('Unknown note type')
 
+	let ratio = (note.type === 4 || note.type === 7) ? 0.6 : 1
 	let horPos = margin.side + noteAreaWidth * note.x
 	let verPos = yPos(page.scan_line_direction, (note.tick-page.start_tick) / (page.end_tick-page.start_tick))
-	ctx.drawImage(noteObj,
-		horPos - noteObj.width *noteRatio/2,
-		verPos - noteObj.height*noteRatio/2,
-		noteObj.width*noteRatio, noteObj.height*noteRatio
-	)
+
+	if (rotate) {
+		ctx.save()
+		ctx.translate(horPos, verPos)
+		ctx.rotate(rotate)
+		ctx.translate(-noteObj.width*noteRatio*ratio/2, -noteObj.height*noteRatio*ratio/2)
+		ctx.drawImage(noteObj, 0, 0, noteObj.width*noteRatio*ratio, noteObj.height*noteRatio*ratio)
+		ctx.restore()
+	} else {
+		ctx.drawImage(noteObj, 
+			horPos - noteObj.width *noteRatio*ratio/2,
+			verPos - noteObj.height*noteRatio*ratio/2,
+			noteObj.width*noteRatio*ratio, noteObj.height*noteRatio*ratio
+		)
+	}
 }
 
 function drawLink(note, page, nextNote, nextPage, ctx) {
@@ -230,19 +278,26 @@ function processData(data, path) {
 		})
 
 		// draw next page
-		ctx.globalAlpha = 0.15
+		let canvas2 = createCanvas(width, height)
+		let ctx2 = canvas2.getContext('2d')
 		nextNotes.forEach(note=>{
-			if (note.type === 2) drawLongHoldTail(note, page_list[page_index+1], ctx)
+			if (note.type === 2) drawLongHoldTail(note, page_list[page_index+1], ctx2)
 		})
 		nextNotes.forEach(note=>{
-			if (note.type === 1) drawHoldTail(note, page_list[page_index+1], ctx)
+			if (note.type === 1) drawHoldTail(note, page_list[page_index+1], ctx2)
 			if (note.next_id > 0) {
 				let nextNote = note_list[note.next_id]
 				let nextPage = page_list[nextNote.page_index]
-				drawLink(note, page_list[page_index+1], nextNote, nextPage, ctx)
+				drawLink(note, page_list[page_index+1], nextNote, nextPage, ctx2)
 			}
 		})
-		nextNotes.forEach(note=>{ drawNote(note, page_list[page_index+1], ctx) })
+		nextNotes.forEach(note=>{
+			let rotate = (note.type === 3) ?
+				getRotate(note, page_list[page_index+1], note_list[note.next_id], page_list[note_list[note.next_id].page_index]) - Math.PI/4 : 0
+			drawNote(note, page_list[page_index+1], ctx2, rotate)
+		})
+		ctx.globalAlpha = 0.1
+		ctx.drawImage(canvas2, 0, 0)
 		ctx.globalAlpha = 1
 
 		// Draw long holds
@@ -259,8 +314,13 @@ function processData(data, path) {
 				let nextPage = page_list[nextNote.page_index]
 				drawLink(note, page, nextNote, nextPage, ctx)
 			}
+			if (note.type !== 4 && note.type !== 7) drawNoteLine(note, page, ctx, data.time_base)
 		})
-		notes.forEach(note=>{ drawNote(note, page, ctx) })
+		notes.forEach(note=>{
+			let rotate = (note.type === 3) ?
+				getRotate(note, page, note_list[note.next_id], page_list[note_list[note.next_id].page_index]) - Math.PI/4 : 0
+			drawNote(note, page, ctx, rotate)
+		})
 
 		fs.writeFileSync(`${path}/${`${page_index}`.padStart(3, '0')}.png`, canvas.toBuffer())
 		//console.log(`Written to ${path}/${page_index}.png`)
